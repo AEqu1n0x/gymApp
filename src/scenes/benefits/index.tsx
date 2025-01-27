0c9ca15d6987ed8e5c08959bcb47e5b370887bd0
AEqu1n0x <oneorov@gmail.com>
2025-01-27 17:42:55 +0700

    add OurClasses page with scroll function

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -99,7 +99,7 @@ const Benefits = ({ setSelectedPage }: Props) => {
           <div>
             {/* Заголовок*/}
             <div className="relative">
-              <div className="before:absolute before:-left-20 before:-top-20 before:z-[1] before:content-abstractwaves">
+              <div className="overflow-hidden before:absolute before:-left-20 before:-top-20 before:z-[1] before:content-abstractwaves">
                 <motion.div
                   initial="hidden"
                   whileInView="visible"
@@ -119,32 +119,33 @@ const Benefits = ({ setSelectedPage }: Props) => {
             </div>
 
             {/* Описание*/}
-
-            <motion.div
-              initial="hidden"
-              whileInView="visible"
-              viewport={{ once: true, amount: 0.5 }}
-              transition={{ delay: 0.3, duration: 0.8 }}
-              variants={{
-                hidden: { opacity: 0, x: 100 },
-                visible: { opacity: 1, x: 0 },
-              }}
-            >
-              <p className="my-5">
-                Nascetur aenean massa auctor tincidunt. Iaculis potenti amet
-                egestas ultrices consectetur adipiscing ultricies enim. Pulvinar
-                fames vitae vitae quis. Quis amet vulputate tincidunt at in
-                nulla nec. Consequat sed facilisis dui sit egestas ultrices
-                tellus. Ullamcorper arcu id pretium sapien proin integer nisl.
-                Felis orci diam odio.
-              </p>
-              <p className="mb-5">
-                Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
-                tellus quam porttitor. Mauris velit euismod elementum arcu neque
-                facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
-                enim mattis odio in risus nunc.
-              </p>
-            </motion.div>
+            <div className="overflow-hidden">
+              <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ delay: 0.3, duration: 0.8 }}
+                variants={{
+                  hidden: { opacity: 0, x: 100 },
+                  visible: { opacity: 1, x: 0 },
+                }}
+              >
+                <p className="my-5">
+                  Nascetur aenean massa auctor tincidunt. Iaculis potenti amet
+                  egestas ultrices consectetur adipiscing ultricies enim.
+                  Pulvinar fames vitae vitae quis. Quis amet vulputate tincidunt
+                  at in nulla nec. Consequat sed facilisis dui sit egestas
+                  ultrices tellus. Ullamcorper arcu id pretium sapien proin
+                  integer nisl. Felis orci diam odio.
+                </p>
+                <p className="mb-5">
+                  Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel
+                  est tellus quam porttitor. Mauris velit euismod elementum arcu
+                  neque facilisi. Amet semper tortor facilisis metus nibh.
+                  Rhoncus sit enim mattis odio in risus nunc.
+                </p>
+              </motion.div>
+            </div>
 
             {/* Кнопка*/}
             <div className="relative mt-16">
